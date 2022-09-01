798a8b8d2df0a033745d6006b6599e73d61429cf
albertonov <72156084+albertonov@users.noreply.github.com>
2022-09-01 09:59:13 +0200

    Update enableWINRM.ps1

--- enableWINRM.ps1
+++ enableWINRM.ps1
@@ -1,1 +1,2 @@
-Start-Process -FilePath 'cmd.exe' -ArgumentList '/c winrm set winrm/config/service @{AllowUnencrypted=\"true\"} && winrm set winrm/config/service/auth @{Basic=\"true\"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
